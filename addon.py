3e62e55aa7964e03f8fdc9ca58e16cff528ccc3c
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-05-12 08:51:06 +0200

    Add files via upload

--- addon.py
+++ addon.py
@@ -1,7 +1,7 @@
 import xbmcaddon
 import xbmcgui
 import os
-
+import os.path
 
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
@@ -11,16 +11,18 @@ addonname   = addon.getAddonInfo('name')
 # directory = "/storage/.kodi/addons/plugin.program.gamestarter"
 
 
-# foo = xbmcplugin.getSetting(,'debug')
-
 # primero habria que comprobar si es la priemra vez que se lanza entonces hacer la instalacion:
-# xbmcgui.Dialog().ok(addonname, "Your are going to install Gamestarter into your OpenELEC system, please do not switch off your Raspberry Pi until installation is finished.")
-# os.system("wget --no-check-certificate -O /storage/install-gamestarter.sh https://raw.githubusercontent.com/bite-your-idols/gamestarter-openelec/master/install-gamestarter.sh && sh /storage/install-gamestarter.sh")
-# xbmcgui.Dialog().ok(addonname, "Installation done, please reboot, copy your roms and system bios and enjoy!")
+instalar = os.path.isfile("/storage/.kodi/addons/plugin.program.gamestarter/resources/bin/installer.sh") 
+
+if instalar:
+	xbmcgui.Dialog().ok(addonname, "This is the first time you run this addon. You are going to install Gamestarter into your system, please do not switch off your Raspberry Pi until installation is finished.")
+	# os.system("wget --no-check-certificate -O /storage/install-gamestarter.sh https://raw.githubusercontent.com/bite-your-idols/gamestarter-openelec/master/install-gamestarter.sh && sh /storage/install-gamestarter.sh")
+	os.system("sh /storage//storage/.kodi/addons/plugin.program.gamestarter/resources/bin/installer.sh")
+	xbmcgui.Dialog().ok(addonname, "Installation done, please reboot, copy your roms and system bios and enjoy!")
 
 
-# las siguientes veves pues directamente lanzariamos RetroArch
-# tambien se podia incluir unas settings para decidir si prefieres lanzar retroarch o emulationstation y asi incluir los 2 addons en 1
+# las siguientes veces directamente lanzariamos RetroArch o Emulationstation
+# launcher = xbmcplugin.getSetting(,'launcher_default')
 
 resultado = xbmcgui.Dialog().yesno("Gamestarter: RetroArch Launcher", "Exit Kodi and run RetroArch?");
 
