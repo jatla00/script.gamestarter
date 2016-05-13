d0f38829fd06cb5164fa814df538d8a7904fb4de
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-05-13 10:52:31 +0200

    Add files via upload

---
+++ resources/bin/install_iarl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script for IARL addon installation 
+wget --no-check-certificate -O /storage/downloads/iarl.tar.gz https://github.com/zach-morris/plugin.program.iarl/archive/master.tar.gz
+tar -xf /storage/iarl.tar.gz -C /storage/.kodi/addons/ -xz
+# rm /storage/.kodi/addons/plugin.program.iarl-master/resources/bin/romlaunch_OE_RPi2.sh
+# wget --no-check-certificate -O /storage/.kodi/addons/plugin.program.iarl-master/resources/bin/romlaunch_OE_RPi2.sh https://raw.githubusercontent.com/bite-your-idols/gamestarter-openelec/master/scripts/iarl/romlaunch_OE_RPi2.sh
+# chmod a+x /storage/.kodi/addons/plugin.program.iarl-master/resources/bin/romlaunch_OE_RPi2.sh
+rm /storage/iarl.tar.gz
