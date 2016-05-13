be5345d41565fa1c125810141dff8cedbf123db0
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-05-13 10:41:53 +0200

    Add files via upload

---
+++ resources/bin/install_gamemaker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script for GameMaker Pi ports installation 
+wget --no-check-certificate -O /storage/install-gamemaker-openelec.sh https://raw.githubusercontent.com/bite-your-idols/gamemaker-openelec/master/gamemaker-openelec.sh
+
+# for more info visit: https://github.com/bite-your-idols/gamemaker-openelec
