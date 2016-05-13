fd43cd4c13c1daa1304faa24ed4ad8b3e67ec9cf
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-05-13 10:47:53 +0200

    Update default.py

--- default.py
+++ default.py
@@ -12,7 +12,7 @@ directory = os.path.dirname(script_file)
 
 # primero habria que comprobar si es la priemra vez que se lanza entonces hacer la instalacion:
 if os.path.isfile(directory+"/resources/bin/setup.sh") == True: 
- 	xbmcgui.Dialog().ok(addonname, "This is the first time you run this addon. We are going to make some firts-time-setup to Gamestarter, please do not switch off your Raspberry Pi until process is finished.")
+ 	xbmcgui.Dialog().ok(addonname, "This is the first time you run Gamestarter. We are going to make a first-time-setup, please do not switch off your Raspberry Pi until process is finished.")
  	os.system("sh "+directory+"/resources/bin/setup.sh")
  	xbmcgui.Dialog().ok(addonname, "Done. Please reboot, copy your roms and system bios and enjoy!")
 
