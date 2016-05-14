66195ec8e62ed9b6d6b3b3faf60611988d145cdf
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-05-14 10:00:59 +0200

    Delete reinstallsetup.sh

--- resources/bin/reinstallsetup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Update script
-
-rm /storage/.kodi/addons/script.gamestarter/resources/bin/installed
-
-kodi-send --action=Notification"(Gamestarter,Please launch addon to reinstall.,2000,/storage/.kodi/addons/script.gamestarter/icon.png)"
