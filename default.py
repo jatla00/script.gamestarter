ca955843b43624a143ba29fe25788ee859f617b1
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-05-14 10:06:11 +0200

    Update default.py

--- default.py
+++ default.py
@@ -25,3 +25,22 @@ else:
 			os.system(directory+"/resources/bin/gamestarter.sh emulationstation")
 		else:
 			os.system(directory+"/resources/bin/gamestarter.sh retroarch")
+
+
+
+
+# import sys
+# import xbmcgui
+# import xbmcplugin
+
+# addon_handle = int(sys.argv[1])
+
+# xbmcplugin.setContent(addon_handle, 'movies')
+
+# url = 'http://localhost/some_video.mkv'
+# li = xbmcgui.ListItem('My First Video!', iconImage='DefaultVideo.png')
+# xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+# xbmcplugin.endOfDirectory(addon_handle)
+
+# # http://kodi.wiki/view/Audio/video_add-on_tutorial
