066abe8143fa5786de474766596e7658bcb83536
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-05-14 09:51:20 +0200

    Update and rename update.py to reinstall.py

--- resources/bin/reinstall.py
+++ resources/bin/reinstall.py
@@ -7,7 +7,7 @@ import xbmcgui
 import os
 
 
-xbmcgui.Dialog().ok("Gamestarter", "Updating Libretro cores, please dont power off your Raspberry Pi until process is done.")
+xbmcgui.Dialog().ok("Gamestarter", "Preparing re-installation setup.")
 
 
 script_file = os.path.realpath(__file__)
@@ -16,5 +16,5 @@ directory = os.path.dirname(script_file)
 # xbmcgui.Dialog().ok("Gamestarter", "Done!")
 # os.system("sh "+directory+"/resources/bin/install_iarl.sh")
 
-os.system("sh  /storage/.kodi/addons/script.gamestarter/resources/bin/update.sh")
+os.system("sh  /storage/.kodi/addons/script.gamestarter/resources/bin/reinstall.sh")
 
